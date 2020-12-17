b497c2e0ee974717a3b3650b37672815997f44bf
류현우 <lldvvbll@nate.com>
2020-12-17 19:05:48 +0900

    Initial commit

---
+++ Source/Encounters/Private/Encounters.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Encounters.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(Encounters);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Encounters, "Encounters" );
---
+++ Source/Encounters/Public/EncountersGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Encounters.h"
+#include "GameFramework/GameModeBase.h"
+#include "EncountersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENCOUNTERS_API AEncountersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
